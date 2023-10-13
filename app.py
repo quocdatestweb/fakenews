79f2c6533ab46bb5a0a4c6c99298931e270f5b65
quocdatestweb <147794210+quocdatestweb@users.noreply.github.com>
2023-10-14 00:00:09 +0700

    Update app.py

--- app.py
+++ app.py
@@ -89,7 +89,7 @@ flow = Flow.from_client_secrets_file(
     client_secrets_file=client_secrets_file,
     scopes=["https://www.googleapis.com/auth/userinfo.profile",
             "https://www.googleapis.com/auth/userinfo.email", "openid"],
-    redirect_uri="http://127.0.0.1:5000/callback"
+    redirect_uri="https://fake-news-detection-b1ew.onrender.com/callback"
 )
 #rồi
 @app.route('/predict_news', methods=['GET', 'POST'])
